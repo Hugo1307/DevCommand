db3aedb527109d0d49373c2b2bfd91e988676a7b
Hugo Gonçalves <hugomiguel_2001_@sapo.pt>
2025-04-22 21:16:50 +0100

    Added support to argument parsers for optional arguments.

--- src/main/java/dev/hugog/minecraft/dev_command/arguments/CommandArgument.java
+++ src/main/java/dev/hugog/minecraft/dev_command/arguments/CommandArgument.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
 
+import java.util.Objects;
+
 @Getter
 @RequiredArgsConstructor
 @Accessors(fluent = true)
@@ -14,4 +16,16 @@ public class CommandArgument {
     private final int position;
     private final Class<? extends CommandArgumentParser<?>> validator;
     private final boolean optional;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CommandArgument that = (CommandArgument) o;
+        return position == that.position && optional == that.optional && Objects.equals(name, that.name) && Objects.equals(description, that.description) && Objects.equals(validator, that.validator);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, position, validator, optional);
+    }
 }
--- src/main/java/dev/hugog/minecraft/dev_command/commands/IDevCommand.java
+++ src/main/java/dev/hugog/minecraft/dev_command/commands/IDevCommand.java
@@ -4,6 +4,7 @@ import dev.hugog.minecraft.dev_command.arguments.parsers.ICommandArgumentParser;
 import dev.hugog.minecraft.dev_command.validation.IAutoValidationConfiguration;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IDevCommand {
 
@@ -11,11 +12,31 @@ public interface IDevCommand {
 
     List<String> onTabComplete(String[] args);
 
+    /**
+     * Get Argument Parser for a mandatory argument at the given position.
+     *
+     * @param argumentPosition the position of the argument
+     * @return the argument parser
+     * @throws IllegalArgumentException if the argument is not present
+     */
     ICommandArgumentParser<?> getArgumentParser(int argumentPosition);
+
+    /**
+     * Get Argument Parser for an optional argument at the given position.
+     *
+     * @param argumentPosition the position of the argument
+     * @return an Optional containing the argument parser if the argument exists, or an empty Optional if not exists
+     */
+    Optional<ICommandArgumentParser<?>> getOptionalArgumentParser(int argumentPosition);
+
     boolean hasPermissionToExecuteCommand();
+
     boolean hasValidArgs();
+
     boolean canSenderExecuteCommand();
+
     boolean performAutoValidation(IAutoValidationConfiguration configuration);
+
     <T> T getDependency(Class<T> dependencyClass);
 
 }
